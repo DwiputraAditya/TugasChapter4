8772a453bbfa4df1b892b093d40cd373790bcd35
Abid Bilal <abiddddbilal@gmail.com>
2023-04-08 14:22:16 +0700

    initial commit

---
+++ src/main/java/com/binartugaschapter4/binartugaschapter4/Binartugaschapter4Application.java
@@ -0,0 +1,13 @@
+package com.binartugaschapter4.binartugaschapter4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Binartugaschapter4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Binartugaschapter4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/binartugaschapter4/binartugaschapter4/Binartugaschapter4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binartugaschapter4.binartugaschapter4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Binartugaschapter4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
