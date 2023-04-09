87e9e772cf466521d88fae8614238b67dc372aee
DwiputraAditya <you@example.com>
2023-04-09 11:42:15 +0700

    Commit Delete Dwiputra

---
+++ TugasChapter4/TugasChapter4/src/main/java/com/kelompok2/tugas/TugasChapter4Application.java
@@ -0,0 +1,13 @@
+package com.kelompok2.tugas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasChapter4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasChapter4Application.class, args);
+	}
+
+}
---
+++ TugasChapter4/TugasChapter4/src/test/java/com/kelompok2/tugas/TugasChapter4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelompok2.tugas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasChapter4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
